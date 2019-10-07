aece1bc8864f97ff7376f49ead80dbf27dd8a23f
Programmerguy101 <mahmed010@citymail.cuny.edu>
2019-10-07 12:29:54 -0400

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Owen Ahmed on 10/7/19.
+//  Copyright © 2019 Owen Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
